5ed7023d107c429ead6f94f02b507c321cb8c9ad
sellernvcu <sellern@vcu.edu>
2017-10-10 13:36:55 -0400

    works, except for checking for certain words

--- PigLatin/PigLatin.cpp
+++ PigLatin/PigLatin.cpp
@@ -4,7 +4,9 @@
 #include "stdafx.h"
 #include <iostream>
 #include <string> 
+#include <sstream>
 #include "stdio.h"
+
 using namespace std;
 void getinputs();
 int translateToPigLatin(string word);
@@ -18,50 +20,57 @@ int main()
 void getinputs() // gets inputs from users
 {
 	string word;
-	cout << "Please enter a word." << endl;
+	cout << "Please enter a word or phrase." << endl;
 	cin >> word;
-	cout << "Your word is " << word << endl;
-	translateToPigLatin(word); // send inputs to translator 
+	cout << "You entered " << word << endl;
+	istringstream phrase(word);
+	while (phrase >> word)
+	{
+		getline(phrase, word, ' ');
+
+	}
+
+	translateToPigLatin(word);// send inputs to translator
 	
 
 }
 
 int translateToPigLatin(string word)
 {
-	
-	char firstletter = word[0];
+
+
 	string pigLatinAppend = "ay";
-	
-	if (word.compare("the") || word.compare("The")|| word.compare("and")|| word.compare("And")|| word.compare("but")|| word.compare("But")|| word.compare("for")|| word.compare("For")|| word.compare("nor")|| word.compare("Nor")|| word.compare("yet")|| word.compare("Yet"))
+
+	if (word.compare("the") || word.compare("The") || word.compare("and") || word.compare("And") || word.compare("but") || word.compare("But") || word.compare("for") || word.compare("For") || word.compare("nor") || word.compare("Nor") || word.compare("yet") || word.compare("Yet"))
 	{
 		cout << "Your word in Pig Latin is " << word << endl;
-		return 0;
+	
 	}
 
-	while (word.length () > 2)
+	if (word.length() <= 2)
 	{
-	 
+		cout << "Your word in Pig Latin is " << word << endl;
+	}
+
+	if (word.length() > 2)
+
+	{
+
+		char firstletter = word[0];
 		if (firstletter == 'a' || firstletter == 'A' || firstletter == 'e' || firstletter == 'E' || firstletter == 'i' || firstletter == 'I' || firstletter == 'o' || firstletter == 'O' || firstletter == 'u' || firstletter == 'U')
-	{
-		cout << "Your word in pig latin is " << word + pigLatinAppend << endl;
-	}
+		{
+			cout << "Your word in pig latin is " << word + pigLatinAppend << endl;
+		}
 
-	else
-	{
-		char lastletter = word[0];
-		string NewWord = word.erase(0, 1) + lastletter + pigLatinAppend;
-		cout << "Your word in pig latin is " << NewWord << endl;
-	}
-
-	}
-	
-	
-	if (word.length () <= 2)
-	{
-		cout << "Your word in Pig Latin is " << word << endl;
+		else
+		{
+			char lastletter = word[0];
+			string NewWord = word.erase(0, 1) + lastletter + pigLatinAppend;
+			cout << "Your word in pig latin is " << NewWord << endl;
+		}
 	}
 	
 	return 0;
-	
+
 }
 
